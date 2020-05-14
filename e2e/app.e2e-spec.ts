1513a96212031a9547745f64674910cc7100bb35
thangaraj80 <thangaraj.thangaraj2@gmail.com>
2020-05-14 14:55:31 +0530

    renamed material theme

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { MaterialDashboardAngularPage } from './app.po';
 
-describe('material-dashboard-angular App', () => {
+describe('covid19 App', () => {
   let page: MaterialDashboardAngularPage;
 
   beforeEach(() => {
