5d0f719e2b3b85398e22c5983469b57581af7498
thangaraj80 <thangaraj.thangaraj2@gmail.com>
2020-05-17 13:51:34 +0530

    [covid19] added datatable

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CoronaApiService } from "./../corona-api.service";
 import { CoronaAllCountries } from "app/classes/corona-all-countries";
+declare const $: any;
 
 @Component({
   selector: 'app-table-list',
@@ -14,14 +15,51 @@ export class TableListComponent implements OnInit {
   constructor(public coronaNews: CoronaApiService) { }
 
   ngOnInit() {
-    this.getAllCountries();
+    this.getAllCountries();  
   }
   getAllCountries() {
-    this.coronaNews.coronaAllCountries().subscribe((results:any[]) => {
-      this.allCountries= results.sort((a, b) => b.cases - a.cases)
-      this.lastUpdate = this.allCountries[0].updated
+    this.coronaNews.coronaAllCountries().subscribe(
+      results => {
+        console.log('Observer got a next value: ' + results)
+        this.allCountries = results.sort((a, b) => b.cases - a.cases)
+      } ,
+      err => {
+        console.error('Observer got an error: ' + err
+        )
+      },
+      () => {
+       
+        $('#covid19-country').DataTable({
+          "bProcessing": true,
+          "bServerSide": true,
+            'bSort': false,
+            
+            "columnDefs": [
+              { "visible": false, "targets": 0 }
+            ],
+      'aoColumns': [
+        {  bSearchable: false, bSortable: true },
+        { bSearchable: true, bSortable: true },
+        {  bSearchable: false, bSortable: true },
+        { bSearchable: false, bSortable: true },
+        { bSearchable: false, bSortable: true },
+        { bSearchable: false, bSortable: true },
+        { bSearchable: true, bSortable: true },
+        { bSearchable: false, bSortable: true },
+        { bSearchable: true, bSortable: true }
+          
 
-    })
-   
+      ],
+    });
+      }
+    )
+    // this.coronaNews.coronaAllCountries().subscribe((results:any[]) => {
+    //   this.allCountries= results.sort((a, b) => b.cases - a.cases)
+    //   this.lastUpdate = this.allCountries[0].updated
+
+    // }
+    
+    // )
+  
   }
 }
